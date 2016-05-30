05b7f56813db65000a033a92d0971c68c59ed963
zachi <zachi.greenberger@gmail.com>
2016-05-30 09:09:31 +0300

    first version

---
+++ CenzorTools/CenzorTools/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CenzorTools
+{
+ class Program
+ {
+  static void Main(string[] args)
+  {
+   int counter = 0;
+   string line;
+
+   // Read the file and display it line by line.
+   System.IO.StreamReader file =
+      new System.IO.StreamReader("data.csv");
+   while ((line = file.ReadLine()) != null)
+   {
+    Console.WriteLine(line);
+    counter++;
+   }
+
+   file.Close();
+
+   // Suspend the screen.
+   Console.ReadLine();
+  }
+ }
+
+ class subject
+ {
+  public string id { get; set; }
+
+  public List<Session> Sessions { get; set; }
+
+
+
+ }
+ class Session { }
+}
